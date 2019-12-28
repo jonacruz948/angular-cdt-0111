81c567abd65713464018947c92a3c40588d2ed1c
Cem <cemdevwork@gmail.com>
2019-12-28 15:25:38 +0300

    28-observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,8 @@
 import { Component } from '@angular/core';
 
-import { Post } from './posts/post.model';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'mean-course';
-  storedPosts: Post[] = [];
-
-  onPostAdded(post: Post) {
-    this.storedPosts.push(post);
-  }
-}
+export class AppComponent {}
