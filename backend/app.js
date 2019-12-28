407305738cd40e270e4ecd4fa85f2df072bcd7e4
Cem <cemdevwork@gmail.com>
2019-12-28 20:28:49 +0300

    http-cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Reqeusted-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-control-Allow-Methods",
+    "GET, POST, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use("/api/posts", (req, res, next) => {
   const posts = [
     {
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 import { Subject } from "rxjs";
 
 import { Post } from "./post.model";
+import { stringify } from 'querystring';
 
 @Injectable({ providedIn: "root" })
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
+    this.http.get<{message: string, posts: Post[]}>("http://localhost:3000/api/posts")
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      })
     return [...this.posts];
   }
 
@@ -17,7 +26,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = { title, content };
+    const post: Post = { id: null, title, content };
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
